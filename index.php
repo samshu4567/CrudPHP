dde81d9ebf46f4c4804095bcc3e6031fce4a6351
samshu4567 <jais.shubham1999@gmail.com>
2024-05-25 15:21:10 +0530

    i change it

--- index.php
+++ index.php
@@ -127,9 +127,7 @@ else{
   </div>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">
-    <img src="/Project/CRUD_PHP/CrudPHP/logo1.jpg" height="38px" alt="">
-</a>
+  <a class="navbar-brand" href="#"><img src="/Project/CRUD_PHP/CrudPHP/logo1.jpg" height="38px" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
